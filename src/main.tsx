44565a76566601a8618edd99b9b72a7bf0017210
peter khalil <peterkhalil@peters-MacBook-Air.local>
2025-08-22 22:01:48 +0300

    chore: trigger refresh

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import CompactDeck from './pages/CompactDeck.tsx'
 import './index.css'
 
+// Trigger refresh
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CompactDeck />
